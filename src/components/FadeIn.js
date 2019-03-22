fedd6052bab5d2eec2be631564a263026a354370
dhilipkmr <dhilip1211@gmail.com>
2019-03-22 23:24:23 +0530

    blogs

--- src/components/FadeIn.js
+++ src/components/FadeIn.js
@@ -4,7 +4,7 @@ export default class FadeIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wrapHeight: props.wrapHeight ? props.wrapHeight: 0
+      wrapHeight: props.wrapHeight ? props.wrapHeight: ''
     };
   }
 
@@ -19,9 +19,15 @@ export default class FadeIn extends React.Component {
   render() {
     const {children, className:compClass = '', style} = this.props;
     const {wrapHeight, hasStyleInfo} = this.state;
+    let transformStyle = {};
+    if (wrapHeight) {
+      transformStyle = {
+        transform: `translateY(${wrapHeight}px)`
+      }
+    }
     return (
-      <div ref="wrap" className={'fadeInWrap ' + (hasStyleInfo ? '' :'vh')} style={{ height: wrapHeight + 'px'}}>
-        <div ref="fadeInWord" className={'fadeInWord ' + compClass} style={{transform: `translateY(${wrapHeight}px)`, ...style}}>
+      <div ref="wrap" className={'fadeInWrap ' + (hasStyleInfo ? '' :'vh')} style={{ height: wrapHeight ? (wrapHeight + 'px') : 'auto'}}>
+        <div ref="fadeInWord" className={'fadeInWord ' + compClass} style={{...transformStyle, ...style}}>
           {children}
         </div>
       </div>
