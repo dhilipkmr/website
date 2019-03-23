2fd33d8fe575eefecec197e805092238f7b90736
dhilipkmr <dhilip1211@gmail.com>
2019-03-23 22:38:08 +0530

    image addition

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -14,7 +14,7 @@ const Avatar = () => {
       <div className="textcenter white padT20 avatarInto lh2em">
         <FadeIn className="padT10" style={{ animationDelay: '0.6s', lineHeight: '2em'}}>a blog by <span><a href={LINKEDIN_URL} target="_blank" className="themeColor linkHand" rel="noopener noreferrer">dhilip</a></span></FadeIn>
         <FadeIn className="padT10" style={{ animationDelay: '0.8s', lineHeight: '2em'}}>front-end developer @<a href={GI_LINKEDIN_URL} target="_blank" rel="noopener noreferrer"><span className="linkHand">goibibo</span></a></FadeIn>
-        <FadeIn className="padT10" style={{ animationDelay: '1s', lineHeight: '2em'}}>accidental blogger | empath | mr.sings a lot | occsasional traveller</FadeIn>
+        <FadeIn className="padT10" style={{ animationDelay: '1s', lineHeight: '2em'}}>amateur blogger | empath | mr.sings a lot | occsasional traveller</FadeIn>
       </div>
     </div>
   );
