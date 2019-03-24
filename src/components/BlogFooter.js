c9b08521bc0c964320bb557412e51a7883fedd5b
dhilipkmr <dhilip1211@gmail.com>
2019-03-24 11:47:46 +0530

    fa

--- src/components/BlogFooter.js
+++ src/components/BlogFooter.js
@@ -3,7 +3,6 @@ import {Link} from 'gatsby';
 
 export default function BlogFooter(props) {
   const {posts} = props;
-  console.log(props);
   return (
     <div className="blogFooter">
       <div className="mw960">
@@ -11,11 +10,11 @@ export default function BlogFooter(props) {
           posts.map((post) => {
             const {title, date, description, timeToRead, path} = post.node.frontmatter;
             return(
-              <div className="cardWrap">
-              <Link to={path} className="cardHeader ellipsis2 fb">{title}</Link>
-              <div className="cardHeadingSub ico12">{date + ' ~ ' + timeToRead + ' min read'}</div>
-              <div className="cardDescription ico14 padT10 ellipsis2 padB10">{description}</div>
-            </div>
+              <div className="cardWrap" key={path}>
+                <Link to={path} className="cardHeader ellipsis2 fb">{title}</Link>
+                <div className="cardHeadingSub ico12">{date + ' ~ ' + timeToRead + ' min read'}</div>
+                <div className="cardDescription ico14 padT10 ellipsis2 padB10">{description}</div>
+              </div>
             )
           })
         }
