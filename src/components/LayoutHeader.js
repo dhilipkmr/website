b6e64b6f45e071bed7ecbdc21b9e4978a03b52f8
dhilipkmr <dhilip1211@gmail.com>
2019-03-30 20:52:58 +0530

    hamburger  menu

--- src/components/LayoutHeader.js
+++ src/components/LayoutHeader.js
@@ -30,11 +30,14 @@ class LayoutHeader extends React.Component{
             <div className="menuContainer">
               <div><MdClose className="themeHover hand menuFont lh2em closeBtn" onClick={()=>{ this.setState({ showMenu: false})}}/></div>
               <div className="transCenter menuHeadingContainer widthOptimize textcenter">
-                <div className="themeHover hand menuFont lh2em">Skills</div>
-                <div className="themeHover hand menuFont lh2em">About Me</div>
-                <div className="themeHover hand menuFont lh2em">Contact</div>
+                <Link to='/'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/' ? 'themeColor' : '')} >Home</div></Link>
+                <Link to='/blogs'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/blogs' ? 'themeColor' : '')}>Blogs</div></Link>
+                <Link to='/portfolio'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/portfolio' ? 'themeColor' : '')}>Portfolio</div></Link>
+                <Link to='/skills'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/skills' ? 'themeColor' : '')}>Skills</div></Link>
+                <Link to='/aboutme'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/aboutme' ? 'themeColor' : '')}>About Me</div></Link>
+                <Link to='/contact'><div className={'themeHover hand menuFont fb lh2em ' + (currPath === '/contact' ? 'themeColor' : '')}>Contact</div></Link>
                 <a href="https://drive.google.com/uc?export=download&id=1VwfM39_eH6ltBjDKqaWvzIhsNkMMiPv4">
-                  <div className="themeHover hand menuFont lh2em"> Resume<FaDownload style={{ paddingTop: '15px'}}/></div>
+                  <div className="themeHover hand menuFont lh2em fb "> Resume<FaDownload style={{ paddingTop: '15px'}}/></div>
                 </a>
               </div>
             </div>
@@ -47,9 +50,9 @@ class LayoutHeader extends React.Component{
               <TiThMenu className="hand fl marginR20 fadeInWord" onClick={this.showMenu}/>
             </div>
             <div className="layoutHeaderwrap marginR5p op0 opAnimator fadeInWord posSticky" style={{ 'animationDelay': '0s'}}>
-              <Link className={'inbl marginR5p ' + (currPath === '/' ? 'fb' : '')} to="/"><div className="">Home</div></Link>
-              <Link className={'inbl marginR5p ' + (currPath === '/blogs' ? 'fb' : '')} to="/blogs"><div className="">Blogs</div></Link>
-              <Link className={'inbl marginR5p ' + (currPath === '/portfolio' ? 'fb' : '')} to='/portfolio'><div className="">Portfolio</div></Link>
+              <Link className={'inbl marginR5p hideless500w ' + (currPath === '/' ? 'fb' : '')} to="/"><div className="">Home</div></Link>
+              <Link className={'inbl marginR5p hideless500w ' + (currPath === '/blogs' ? 'fb' : '')} to="/blogs"><div className="">Blogs</div></Link>
+              <Link className={'inbl marginR5p hideless500w ' + (currPath === '/portfolio' ? 'fb' : '')} to='/portfolio'><div className="">Portfolio</div></Link>
               {/* <Link className={'inbl marginR5p ' + (currPath === '/aboutme' ? 'fb' : '')} to='/aboutme'><div className="">About Me</div></Link> */}
               <Toggle theme={theme} themer={themer}/>
             </div>
