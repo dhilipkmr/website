ecc04178cdfd19f91e11de9b14c47f8da7c2b83a
dhilipkmr <dhilip1211@gmail.com>
2019-04-02 17:49:49 +0530

    emoji

--- src/components/constants.js
+++ src/components/constants.js
@@ -89,10 +89,10 @@ export const technicalSkills2 = [
 
 export const introduction = {
   'description': ['> Hello there!',
-                '> I\'m Dhilip Kumar, as you know already :P',
+                '> I\'m Dhilip Kumar, as you know already 😅',
                 '> I am a Software Professional, working as a Front-end Developer at Goibibo.',
-                '> I use Javascript, React, Redux and Node Extensively and create a Performant UI.',
-                '> I also Blog about some Interesting (Technical) stuffs that I create.',
+                '> I use Javascript, React, Redux and Node to create a Performant UI.',
+                '> I also Blog about some Interesting (Technical) stuffs that I create at DEV community',
                 '> I have few Open source projects and Npm Packages if you are interested :)',
                 '> What other things I do you ask?',
                 '> Well...',
